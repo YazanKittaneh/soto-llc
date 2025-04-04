27094256df76aa901d38dcc0c3b97b8b211b5cd1
Yazan Kittaneh <yazanakittaneh@gmail.com>
2025-04-04 17:36:42 -0500

    refactor: remove unused supabaseAdmin export

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,4 +5,3 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
